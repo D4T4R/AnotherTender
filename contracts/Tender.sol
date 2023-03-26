9a81a61b3b8b1f70e74aea6a5291276ed7f27352
AD <aniketdatar@cyberservices.com>
2023-03-26 19:05:33 +0530

    Added Area field to contract

--- contracts/Tender.sol
+++ contracts/Tender.sol
@@ -31,8 +31,7 @@ contract TenderAuction {
         string itemName;
         string itemDescription;
         uint quantity;
-        string area;
-        //uint deadline;
+        string area; 
         address userHash;
     }
 
@@ -80,9 +79,9 @@ contract TenderAuction {
         bidders[msg.sender] = Bidder(bidderCount, _username);
     }
 
-    function createTender(string memory _itemName, string memory _itemDescription, uint _quantity, string memory _area /*uint _deadline*/) public {
+    function createTender(string memory _itemName, string memory _itemDescription, uint _quantity, string memory _area) public {
         tenderCount++;
-        tenders[tenderCount] = Tender(tenderCount, _itemName, _itemDescription, _quantity, _area, /*_deadline,*/ msg.sender);
+        tenders[tenderCount] = Tender(tenderCount, _itemName, _itemDescription, _quantity, _area, msg.sender);
     }
 
     function createBid(uint _tenderId, uint _bid) public {
